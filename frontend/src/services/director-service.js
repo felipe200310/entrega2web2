253403913ba361f30fb75369373331761d03042f
felipe200310 <cristianfelipesilva10@hotmail.com>
2026-03-29 18:15:09 -0500

    Update API base URL to production in frontend services (again)

--- frontend/src/services/director-service.js
+++ frontend/src/services/director-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:4000/api' });
+const api = axios.create({ baseURL: 'https://backend-4bh4.onrender.com/api' });
 
 export const obtenerDirectores = () => api.get('/directores');
 export const crearDirector = (datos) => api.post('/directores', datos);
--- frontend/src/services/productora-service.js
+++ frontend/src/services/productora-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:4000/api' });
+const api = axios.create({ baseURL: 'https://backend-4bh4.onrender.com/api' });
 
 export const obtenerProductoras = () => api.get('/productoras');
 export const crearProductora = (datos) => api.post('/productoras', datos);
